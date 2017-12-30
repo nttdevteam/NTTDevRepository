722e4c2f4c95f3863fe47d5b8f9cea28ed6a07a2
Onkarnath <onkarnath.rai@nttdata.com>
2017-12-30 19:28:49 +0530

    changes made in testGit.java

---
+++ NttDummyProject/src/TestGit.java
@@ -0,0 +1,12 @@
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		String names[] = {"Virat","Rohit"};
+		
+		for(int i=0; i<names.length; i++) {
+			System.out.println(names[i]);
+		}
+	}
+
+}
