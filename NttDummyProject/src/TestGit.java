e34623cc8d3083a204c66f9accc027e75c27175e
Onkarnath <onkarnath.rai@nttdata.com>
2017-12-30 19:51:15 +0530

    first commit

--- NttDummyProject/src/TestGit.java
+++ NttDummyProject/src/TestGit.java
@@ -4,7 +4,7 @@ public class TestGit {
 	public static void main(String[] args) {
 		String names[] = {"Virat","Rohit"};
 		
-		for(int i=0; i<names.length; i++) {
+		for(int i=0;i<names.length; i++) {
 			System.out.println(names[i]);
 		}
 	}
