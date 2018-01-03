13fe99e6bd9546abc843737c498f50e24d924f72
Onkarnath <onkarnath.rai@nttdata.com>
2018-01-03 11:23:05 +0530

    an extra name added by Dev1

--- NttDummyProject/src/TestGit.java
+++ NttDummyProject/src/TestGit.java
@@ -2,7 +2,7 @@
 public class TestGit {
 
 	public static void main(String[] args) {
-		String names[] = {"Virat","Rohit","Shikhar","Ashwin"};
+		String names[] = {"Virat","Rohit","Shikhar","Ashwin","Pujara"};
 		
 		for(int i=0; i<names.length; i++) {
 			System.out.println(names[i]);
